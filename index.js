4a3bd3c525d0982cda3c0c11b0bf3a157f7f4824
vldmkr <vladmakaryan@gmail.com>
2016-10-05 19:33:20 +0300

    parsing package info with aapt implemented

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 'use strict';
 
-const path   = require("path");
-const os     = require("os");
+const exec   = require('child_process').exec;
+const path   = require('path');
+const os     = require('os');
 const fs     = require('fs');
 
-fs.access(path.join(__dirname, 'lib', os.type(), 'aapt'), fs.X_OK, err => {
-  if(err) {
-    console.log('Hmmm, what OS are you using?', os.type(), os.EOL, err); 
-  } else {
-    console.log("lets rock");
-  } 
-});
+const aapt = path.join(__dirname, 'lib', os.type(), 'aapt');
+
+module.exports = function (filename, callback) {
+  fs.access(aapt, fs.X_OK, err => {
+    if(err) {
+      err.msg = ['Hmmm, what OS are you using?', os.type()].join(' ');
+      callback(null, err);
+    } else {
+      const cmd = [aapt, 'dump', 'badging', filename, '|', 'grep', 'package'].join(' ');
+      exec(cmd, (err, stdout, stderr) => {
+        var error = err || stderr;
+        if(error) {
+          callback(null, error);
+        } else {
+          const match = stdout.match(/name='([^']+)'[\s]*versionCode='(\d+)'[\s]*versionName='([^']+)/);
+          const info = {
+            packageName : match[1],
+            versionCode : match[2],
+            versionName : match[3],
+          }
+          callback(info, null);
+        }
+      });
+    } 
+  });
+}
