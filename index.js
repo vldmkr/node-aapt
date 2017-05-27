da461fb832d25a24056fb0efd7773666fb80d539
Xuezheng Ma <xuezhma@gmail.com>
2017-05-26 18:21:17 -0700

    Add support for Promise

--- index.js
+++ index.js
@@ -8,26 +8,32 @@ const fs     = require('fs');
 const aapt = path.join(__dirname, 'lib', os.type(), 'aapt');
 
 module.exports = function (filename, callback) {
-  fs.access(aapt, fs.X_OK, (err) => {
-    if(err) {
-      err.msg = ['Hmmm, what OS are you using?', os.type()].join(' ');
-      callback(err, null);
-    } else {
-      const cmd = [aapt, 'dump', 'badging', filename, '|', 'grep', 'package'].join(' ');
-      exec(cmd, (err, stdout, stderr) => {
-        const error = err || stderr;
-        if(error) {
-          callback(error, null);
-        } else {
-          const match = stdout.match(/name='([^']+)'[\s]*versionCode='(\d+)'[\s]*versionName='([^']+)/);
-          const info = {
-            packageName : match[1],
-            versionCode : match[2],
-            versionName : match[3],
-          };
-          callback(null, info);
-        }
-      });
-    } 
+  callback = callback || function () {}
+  return new Promise(function (resolve, reject) {
+    fs.access(aapt, fs.X_OK, (err) => {
+      if(err) {
+        err.msg = ['Hmmm, what OS are you using?', os.type()].join(' ');
+	    reject(err);
+        callback(err, null);
+      } else {
+        const cmd = [aapt, 'dump', 'badging', filename, '|', 'grep', 'package'].join(' ');
+        exec(cmd, (err, stdout, stderr) => {
+          const error = err || stderr;
+          if(error) {
+		    reject(error);
+            callback(error, null);
+          } else {
+            const match = stdout.match(/name='([^']+)'[\s]*versionCode='(\d+)'[\s]*versionName='([^']+)/);
+            const info = {
+              packageName : match[1],
+              versionCode : match[2],
+              versionName : match[3],
+            };
+	  	  resolve(info);
+            callback(null, info);
+          }
+        });
+      }
+    });
   });
 };
