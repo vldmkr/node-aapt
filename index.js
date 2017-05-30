3dbb8de072b88f9faba156c761be8fe4385377ed
vldmkr <vladmakaryan@gmail.com>
2017-05-30 13:49:38 +0300

    cleaning up the code

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const fs     = require('fs');
 const aapt = path.join(__dirname, 'lib', os.type(), 'aapt');
 
 module.exports = function (filename, callback) {
-  callback = callback || function () {}
+  callback = callback || function () {};
   return new Promise(function (resolve, reject) {
     fs.access(aapt, fs.X_OK, (err) => {
       if(err) {
         err.msg = ['Hmmm, what OS are you using?', os.type()].join(' ');
-	    reject(err);
+        reject(err);
         callback(err, null);
       } else {
         const cmd = [aapt, 'dump', 'badging', filename, '|', 'grep', 'package'].join(' ');
         exec(cmd, (err, stdout, stderr) => {
           const error = err || stderr;
           if(error) {
-		    reject(error);
+            reject(error);
             callback(error, null);
           } else {
             const match = stdout.match(/name='([^']+)'[\s]*versionCode='(\d+)'[\s]*versionName='([^']+)/);
@@ -29,7 +29,7 @@ module.exports = function (filename, callback) {
               versionCode : match[2],
               versionName : match[3],
             };
-	  	  resolve(info);
+            resolve(info);
             callback(null, info);
           }
         });
